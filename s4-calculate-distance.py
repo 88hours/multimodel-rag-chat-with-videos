606b71c8a30c33ff745f6b538107d0704a1fb496
Nomi <itsnomihere@gmail.com>
2025-02-05 14:36:49 +1100

    Calculate Cosine and Euclidean Distance

--- s4-calculate-distance.py
+++ s4-calculate-distance.py
@@ -2,6 +2,7 @@ import numpy as np
 from numpy.linalg import norm
 import torch
 from IPython.display import display
+import cv2
 
 url1='http://farm3.staticflickr.com/2519/4126738647_cc436c111b_z.jpg'
 cap1='A motorcycle sits parked across from a herd of livestock'
@@ -37,26 +38,46 @@ def load_tensor(path):
     return torch.load(path)
 
 def load_embeddings():
-    img1_tensor = load_tensor(img1['tensor_path'] + '.pt')
-    img2_tensor = load_tensor(img2['tensor_path'] + '.pt')
-    img3_tensor = load_tensor(img3['tensor_path'] + '.pt')
-    return img1_tensor.data.numpy(), img2_tensor.data.numpy(), img3_tensor.data.numpy()
+    ex1_embed = load_tensor(img1['tensor_path'] + '.pt')
+    ex2_embed = load_tensor(img2['tensor_path'] + '.pt')
+    ex3_embed = load_tensor(img3['tensor_path'] + '.pt')
+    return ex1_embed.data.numpy(), ex2_embed.data.numpy(), ex3_embed.data.numpy()
 
 def cosine_similarity(vec1, vec2):
     similarity = np.dot(vec1,vec2)/(norm(vec1)*norm(vec2))
     return similarity
 
-def calculate_distance():
-    img1_tensor, img2_tensor, img3_tensor = load_embeddings()
-    similarity1 = cosine_similarity(img1_tensor, img2_tensor)
-    similarity2 = cosine_similarity(img1_tensor, img3_tensor)
-    similarity3 = cosine_similarity(img2_tensor, img3_tensor)
+def calculate_cosine_distance():
+    ex1_embed, ex2_embed, ex3_embed = load_embeddings()
+    similarity1 = cosine_similarity(ex1_embed, ex2_embed)
+    similarity2 = cosine_similarity(ex1_embed, ex3_embed)
+    similarity3 = cosine_similarity(ex2_embed, ex3_embed)
     return [similarity1, similarity2, similarity3]
 
-distances = calculate_distance()
-print("Cosine similarity between ex1_embeded and ex2_embeded is:")
-display(distances[0])
-print("Cosine similarity between ex1_embeded and ex3_embeded is:")
-display(distances[1])
-print("Cosine similarity between ex2_embeded and ex2_embeded is:")
-display(distances[2])
+def calcuate_euclidean_distance():
+    ex1_embed, ex2_embed, ex3_embed = load_embeddings()
+    distance1 = cv2.norm(ex1_embed,ex2_embed, cv2.NORM_L2)
+    distance2 = cv2.norm(ex1_embed,ex3_embed, cv2.NORM_L2)
+    distance3 = cv2.norm(ex2_embed,ex3_embed, cv2.NORM_L2)
+    return [distance1, distance2, distance3]
+
+def show_cosine_distance():
+    distances = calculate_cosine_distance()
+    print("Cosine similarity between ex1_embeded and ex2_embeded is:")
+    display(distances[0])
+    print("Cosine similarity between ex1_embeded and ex3_embeded is:")
+    display(distances[1])
+    print("Cosine similarity between ex2_embeded and ex2_embeded is:")
+    display(distances[2])
+
+def show_euclidean_distance():
+    distances = calcuate_euclidean_distance()
+    print("Euclidean distance between ex1_embeded and ex2_embeded is:")
+    display(distances[0])
+    print("Euclidean distance between ex1_embeded and ex3_embeded is:")
+    display(distances[1])
+    print("Euclidean distance between ex2_embeded and ex2_embeded is:")
+    display(distances[2])
+
+show_cosine_distance()
+show_euclidean_distance()
