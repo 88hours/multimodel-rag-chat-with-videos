14e302a02a5aed440909b7505abdff329d4eff96
Nauman J Qazi <nauman.qazi@systematicbytes.com>
2025-02-24 21:42:44 +1100

    Workaround Gradio UI

--- main_demo.py
+++ main_demo.py
@@ -6,43 +6,44 @@ import ollama
 from utility import download_video, get_transcript_vtt, extract_meta_data
 vid_dir = "./shared_data/videos/yt_video"
 Path(vid_dir).mkdir(parents=True, exist_ok=True)
-metadatas=[]
+the_metadatas=[]
 
 def get_metadata_of_yt_video_with_captions(vid_url):  
     vid_filepath = download_video(vid_url, vid_dir)
     vid_transcript_filepath = get_transcript_vtt(vid_url, vid_dir)
-    global metadatas
     metadatas = extract_meta_data(vid_dir, vid_filepath, vid_transcript_filepath) #should return lowercase file name without spaces
-    return vid_filepath
+    return vid_filepath, metadatas
 
-def init_chat_ui(vid_filepath):
-    with gr.Blocks():        
-        video = gr.Video(vid_filepath)
-        chatbox = gr.Textbox(label="What question do you want to ask?")
-        response = gr.Textbox(label="Response", interactive=False)
-        def chat_response_llvm(instruction):
-            file_path = metadatas['video_path']
-            result = ollama.generate(
-                model='llava',
-                prompt=instruction,
-                images=[file_path],
-                stream=True
-            )['response']
-            print(result)
-            return result
-        
-        chatbox.change(fn=chat_response_llvm, inputs=chatbox, outputs=response)
-
+def chat_response_llvm(instruction):
+    print("Metadatas: ", the_metadatas)
+    #file_path = the_metadatas[0]
+    file_path = 'shared_data/videos/yt_video/extracted_frame/'
+    result = ollama.generate(
+        model='llava',
+        prompt=instruction,
+        images=[file_path],
+        stream=True
+    )['response']
+    return result
     
 def process_url_and_init(youtube_url):
-    vid_filepath = get_metadata_of_yt_video_with_captions(youtube_url)
-    return init_chat_ui(vid_filepath)
+    vid_filepath, metadatas = get_metadata_of_yt_video_with_captions(youtube_url)
+    the_metadatas = metadatas
+    return vid_filepath, vid_filepath
 
 def init_ui():
     with gr.Blocks() as demo:
         url_input = gr.Textbox(label="Enter YouTube URL", value="https://www.youtube.com/watch?v=7Hcg-rLYwdM")
+        video_path = gr.Textbox(interactive=False)
         submit_btn = gr.Button("Process Video")
-        submit_btn.click(fn=process_url_and_init, inputs=url_input, outputs=None)
+        #vid_filepath = 'shared_data/videos/yt_video/Welcome_back_to_Planet_Earth.mp4'
+        chatbox = gr.Textbox(label="What question do you want to ask?", value="What is the astronaut doing?")
+        response = gr.Textbox(label="Response", interactive=False)
+        video = gr.Video()
+        submit_btn2 = gr.Button("ASK")
+        
+        submit_btn.click(fn=process_url_and_init, inputs=url_input, outputs=[video_path,video])
+        submit_btn2.click(fn=chat_response_llvm, inputs=[chatbox], outputs=[response])        
     return demo
 
 if __name__ == '__main__':
